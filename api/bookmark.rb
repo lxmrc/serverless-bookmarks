24e0d5fa02d553ef2f012890d128796ea0c16d76
Alex Mercea <alex@lxmrc.com>
2021-08-08 14:35:22 +1000

    Test returning request origin

--- api/bookmark.rb
+++ api/bookmark.rb
@@ -3,5 +3,5 @@ Handler = Proc.new do |req, res|
   res['Content-Type'] = 'text/html; charset=utf-8'
   res['Access-Control-Allow-Origin'] = '*'
   url = req.query["url"]
-  res.body = url
+  res.body = req.header["Origin"]
 end
