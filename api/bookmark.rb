52ad3f9eb96dfa43df982bd1c1ad2438a7a6e444
Alex Mercea <alex@lxmrc.com>
2021-08-08 14:28:53 +1000

    Allow CORS

--- api/bookmark.rb
+++ api/bookmark.rb
@@ -1,6 +1,7 @@
 Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/html; charset=utf-8'
+  res['Access-Control-Allow-Origin'] = '*'
   url = req.query["url"]
   res.body = url
 end
