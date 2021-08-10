5dff3e4eda2fd355b7824ded75720de82511800a
Alex Mercea <alex@lxmrc.com>
2021-08-10 20:35:50 +1000

    Add 'read later' option

--- api/bookmark.rb
+++ api/bookmark.rb
@@ -1,6 +1,6 @@
-require 'rubygems'
-require 'httparty'
-require 'json'
+require "rubygems"
+require "httparty"
+require "json"
 
 class Editor
   include HTTParty
@@ -8,19 +8,19 @@ class Editor
 
   attr_accessor :repo, :branch, :author
 
-  base_uri 'https://api.github.com'
-  basic_auth ENV['GITHUB_USERNAME'], ENV['GITHUB_TOKEN']
-  default_params :output => 'json'
+  base_uri "https://api.github.com"
+  basic_auth ENV["GITHUB_USERNAME"], ENV["GITHUB_TOKEN"]
+  default_params :output => "json"
   format :json
 
-  def initialize(repo, branch = 'master')
+  def initialize(repo, branch = "master")
     @repo = repo
     @branch = branch
     @author = false
   end
 
   def set_author(name, email)
-    @author = {'name' => name, 'email' => email}
+    @author = {"name" => name, "email" => email}
   end
 
   def update_file(file, message, content)
@@ -40,11 +40,11 @@ class Editor
   end
 
   def last_commit
-    Editor.get("/repos/#{@repo}/git/refs/heads/#{@branch}").parsed_response['object']['sha']
+    Editor.get("/repos/#{@repo}/git/refs/heads/#{@branch}").parsed_response["object"]["sha"]
   end
 
   def tree_for(commit_sha)
-    Editor.get("/repos/#{@repo}/git/commits/#{commit_sha}").parsed_response['tree']['sha']
+    Editor.get("/repos/#{@repo}/git/commits/#{commit_sha}").parsed_response["tree"]["sha"]
   end
 
   def create_tree(file, content, last_tree)
@@ -52,34 +52,35 @@ class Editor
       "base_tree" => last_tree,
       "tree" => [{"path" => file, "mode" => "100644", "type" => "blob", "content" => content}]
     }
-    Editor.post("/repos/#{@repo}/git/trees", body: new_tree.to_json).parsed_response['sha']
+    Editor.post("/repos/#{@repo}/git/trees", body: new_tree.to_json).parsed_response["sha"]
   end
 
   def create_commit(message, tree_sha, parent)
-    commit = { 'message' => message, 'parents' => [parent], 'tree' => tree_sha }
+    commit = { "message" => message, "parents" => [parent], "tree" => tree_sha }
     if @author
-      commit['author'] = @author
+      commit["author"] = @author
     end
-    Editor.post("/repos/#{@repo}/git/commits", :body => commit.to_json).parsed_response['sha']
+    Editor.post("/repos/#{@repo}/git/commits", :body => commit.to_json).parsed_response["sha"]
   end
 
   def update_branch(new)
-    ref = {'sha' => new}
+    ref = {"sha" => new}
     post = Editor.post("/repos/#{@repo}/git/refs/heads/#{@branch}", :body => ref.to_json)
-    post.headers['status'] == '200 OK'
+    post.headers["status"] == "200 OK"
   end
 end
 
-editor = Editor.new('lxmrc/lxmrc.com')
+editor = Editor.new("lxmrc/lxmrc.com")
 
 Handler = Proc.new do |req, res|
   title = req.query["title"]
   url = req.query["url"]
+  list = req.query["list"]
   yaml = <<~YAML
   - title: #{title}
     url: #{url}
   YAML
-  editor.update_file('_data/bookmarks.yml', 'Add bookmark', "#{yaml}")
+  editor.update_file("_data/#{list}.yml", "Add bookmark", "#{yaml}")
   res.status = 200
-  res['Access-Control-Allow-Origin'] = '*'
+  res["Access-Control-Allow-Origin"] = "*"
 end
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,21 @@
+// bookmark
 javascript: (() => {
   const Http = new XMLHttpRequest();
   const pageUrl = window.location.href;
   const pageTitle = document.title;
-  const url='https://serverless-bookmarks.vercel.app/api/bookmark?url=' + pageUrl + '&title=' + pageTitle;
+  const url='https://serverless-bookmarks.vercel.app/api/bookmark?url=' + pageUrl + '&title=' + pageTitle + '&list=bookmarks';
+
+  Http.open("GET", url);
+  Http.send();
+})();
+
+
+// read later
+javascript: (() => {
+  const Http = new XMLHttpRequest();
+  const pageUrl = window.location.href;
+  const pageTitle = document.title;
+  const url='https://serverless-bookmarks.vercel.app/api/bookmark?url=' + pageUrl + '&title=' + pageTitle + '&list=read_later';
 
   Http.open("GET", url);
   Http.send();
